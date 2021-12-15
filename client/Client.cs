3fb5d17c65fc3483206e3f1ce0302545b812b44d
Sergey <seregio2002@outlook.com>
2021-12-15 08:14:11 +0700

    stable ver0.2

--- client/Client.cs
+++ client/Client.cs
@@ -127,7 +127,6 @@ namespace console_snake
             {
                 //вывести сообщение об ошибке
                 altcons.WriteLine(ex.Message);
-
             }
             finally
             {
