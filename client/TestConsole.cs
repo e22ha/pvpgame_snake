4230488d458f54a0c24f69758ed599e9fbd03474
Sergey Tychenko <58304169+SergeyTy@users.noreply.github.com>
2021-12-22 10:50:22 +0700

    Update TestConsole.cs

--- client/TestConsole.cs
+++ client/TestConsole.cs
@@ -1,26 +1,41 @@
 using NUnit.Framework;
+using console_snake;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-
-namespace console_snake
+namespace TestClient;
+[TestFixture]
+public class Tests
 {
-    // атрибут, указывающий на то, что это класс содержит тесты
-    [TestFixture]
-    class TestConsole
+    private Sounds cs;
+    private Client p;
+
+    [SetUp]
+    public void Setup1()
     {
+        cs = new Sounds();
+        Sounds.fullPath = ".wav/icon_06.vaw";
+    }
 
-        [TestCase]
+    [Test]
+    public void Test1()
+    {
+        var exception = Assert.Throws<ArgumentException>(() => Sounds.EatSound());
+        Assert.That(exception.Message, Is.EqualTo("not wav"));
+    }
 
-        {
-            var exception = Assert.Throws<ArgumentException>(() => );
-            Assert.That(exception.Message, Is.EqualTo(""));
-        }
+    [SetUp]
+    public void Setup2()
+    {
+        p = new Client();
+    }
 
-
+    [TestCase("127.0.0.1", 8888)]
+    public void Test2(string adress, int port)
+    {
+        string g = new Guid().ToString(); 
+        //var exception = Assert.Throws<ArgumentException>(() => p.connect_(adress, port, g));
+        //Assert.That(exception, Is.EqualTo(""));
+        Assert.DoesNotThrow(() => p.connect_(adress, port, g));
 
     }
 }
